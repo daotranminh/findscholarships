d07978774664db1b26b7e18049864dcd5bdc01e0
Dao Tran Minh <daotranminh@gmail.com>
2014-03-09 00:24:49 +0100

    Start working on fetching.

---
+++ include/fetch/Fetcher.hpp
@@ -0,0 +1,13 @@
+#ifndef ___FETCHER_HPP___
+#define ___FETCHER_HPP___
+
+#include <string>
+
+class Fetcher
+{
+public:
+  std::string
+  fetch(const std::string &url);
+};
+
+#endif // ___FETCHER_HPP___

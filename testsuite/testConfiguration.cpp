0469d21afc3c250fff4a32a8ffe5217da3654efe
Dao Tran Minh <daotranminh@gmail.com>
2014-03-12 22:52:53 +0100

    Add example for make check.

---
+++ testsuite/testConfiguration.cpp
@@ -0,0 +1,21 @@
+#include "utilities/Configuration.hpp"
+#include "utilities/Logger.hpp"
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE "testConfiguration"
+#include <boost/test/unit_test.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/bind.hpp>
+
+BOOST_AUTO_TEST_CASE ( testConfiguration )
+{
+  DBGINIT(std::cerr, Logger::INFO | Logger::ERRO | Logger::VERB | Logger::DEBU)
+
+  std::string config_filename = "/Users/minhdt/Documents/Study/findscholarships-website/config.cfg";
+  std::string path_root = "/Users/minhdt/Documents/Study/findscholarships-website/";
+  Configuration *config = Configuration::instance();
+
+  config->readConfig(config_filename);
+
+  BOOST_CHECK_EQUAL(config->pathRoot(), path_root);
+}

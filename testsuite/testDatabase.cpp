994c0353ba3aaca126b25396a1e0db18567a755b
Dao Tran Minh <daotranminh@gmail.com>
2014-04-08 12:16:16 +0200

    Update testDatabase: now data entries insertion tested.

--- testsuite/testDatabase.cpp
+++ testsuite/testDatabase.cpp
@@ -39,6 +39,55 @@ createDummyDatabase(const std::size_t db_index)
 }
 
 
+
+void
+insertDummy(Database &db,
+	    std::size_t db_index)
+{
+  // create 1 expired data entry and 1 still valid
+  Date_t today = boost::gregorian::day_clock::local_day();
+  boost::gregorian::months one_month(1);
+  boost::gregorian::months two_months(2);
+  
+  Date_t past_2_months = today - two_months;
+  Date_t in_1_month = today + one_month;
+
+  DatePtr past_2_months_ptr = std::make_shared<Date_t>(past_2_months);
+  DatePtr in_1_month_ptr = std::make_shared<Date_t>(in_1_month);
+
+  Configuration *config = Configuration::instance();
+  const StringVec &categories = config->categories();
+  const std::string category = categories[db_index];
+
+  Title title_expired(category + " expired 222"); 
+  std::string link_expired = "http://www." + category + ".expired222.html";
+
+  Title title_valid(category + " valid 222");
+  std::string link_valid = "http://www." + category + ".valid222.html";
+
+  db.insert(db_index, past_2_months_ptr, title_expired, link_expired);
+  db.insert(db_index, in_1_month_ptr, title_valid, link_valid);
+}
+
+
+
+void
+check_file_length(std::size_t db_index)
+{
+  Configuration *config = Configuration::instance();
+  const StringVec &path_storages = config->pathStorages();
+  const StringVec &categories = config->categories();
+  const std::string filename = path_storages[db_index];
+  const std::string category = categories[db_index];
+
+  std::size_t number_entries;
+  if (db_index < TOTAL_STORAGES - 1)
+    number_entries = 2;
+  else
+    number_entries = 12;
+}
+
+
 BOOST_AUTO_TEST_CASE ( testDatabase )
 {
   DBGINIT(std::cerr, Logger::INFO | Logger::VERB | Logger::DEBU)
@@ -48,14 +97,14 @@ BOOST_AUTO_TEST_CASE ( testDatabase )
   config->readConfig(config_filename);
 
   // create some dummy database for loading
-  for (std::size_t i = 0; i < TOTAL_STORAGES; ++i)
+  for (std::size_t i = 0; i < TOTAL_STORAGES-1; ++i)
     {
       createDummyDatabase(i);
     }
 
+  // test loading database ******************************************************
   Database db;
   db.loadDatabase();
-  //db.showDatabase();
 
   const StorageVec &storages = db.getStorages();
   StorageVec::const_iterator homepage_it = storages.end();
@@ -69,6 +118,27 @@ BOOST_AUTO_TEST_CASE ( testDatabase )
     }
 
   // the homepage database of course has more entries
-  const StoragePtr s = *homepage_it;
-  BOOST_CHECK_EQUAL(s->size(), storages.size());
+  const StoragePtr homepage_storage = *homepage_it;
+  BOOST_CHECK_EQUAL(homepage_storage->size(), storages.size()-1);
+
+  // test inserting data entries ************************************************
+  for (std::size_t i = 0; i < TOTAL_STORAGES-1; ++i)
+    {
+      insertDummy(db, i);
+    }
+
+  // now each category should have 2 entries (corresponding to the valid ones)
+  for (StorageVec::const_iterator it = storages.begin(); it != homepage_it; ++it)
+    {
+      const StoragePtr s = *it;
+      BOOST_CHECK_EQUAL(s->size(), 2);
+    }
+  BOOST_CHECK_EQUAL(homepage_storage->size(), 2*(storages.size()-1));
+
+  // test storing database *****************************************************
+  db.storeDatabase();
+  for (std::size_t i = 0; i < TOTAL_STORAGES; i++)
+    {
+      check_file_length(i);
+    }
 }

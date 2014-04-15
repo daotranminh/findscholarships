f1d7c8156e965f4b50c6cfe60121c37001d16849
Dao Tran Minh <daotranminh@gmail.com>
2014-04-15 21:37:14 +0200

    Add find scholarship binary.

---
+++ src/findscholarship.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include "utilities/Configuration.hpp"
+#include "utilities/HelperFunctions.hpp"
+#include "utilities/Logger.hpp"
+#include "htmlgen/Database.hpp"
+#include "htmlgen/HtmlGenBase.hpp"
+#include "htmlgen/Manager.hpp"
+#include "htmlgen/Title.hpp"
+
+
+int 
+main (int argc, const char *argv[])
+{
+  DBGINIT(std::cerr, Logger::INFO | Logger::ERRO | Logger::VERB | Logger::DEBU)
+	
+  std::string config_filename = "/Users/minhdt/Documents/Study/findscholarships-website/config.cfg";
+  std::string input_filename = "/Users/minhdt/Documents/Study/findscholarships-website/tmp/crawled_input.txt";
+  
+  DBGINFO("Read config")
+    
+  Configuration* config = Configuration::instance();
+  config->readConfig(config_filename);
+  
+  DBGINFO("Load db")
+    
+  Database db;
+  db.loadDatabase();
+  
+  DBGINFO("Process batch")
+  Manager man;	
+  man.processBatch(input_filename, db);
+  
+  DBGINFO("Finish!")
+    
+  return 0;
+}

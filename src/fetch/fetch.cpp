94e9e49e9300ea52328cd58d3808c1538a95fdf4
Dao Tran Minh <daotranminh@gmail.com>
2014-05-07 22:59:46 +0200

    Use boost::program_options to allow fetching from manual input or dbworld.

--- src/fetch/fetch.cpp
+++ src/fetch/fetch.cpp
@@ -1,12 +1,36 @@
 #include <string>
 
+#include <boost/program_options.hpp>
+
 #include "fetch/Fetcher.hpp"
 #include "utilities/Configuration.hpp"
 #include "utilities/Logger.hpp"
+#include "utilities/ProgramOptions.hpp"
 
 
-int main()
+int main(int argc, char *argv[])
 {
+  std::string fetch_from = "all";
+
+  const char *help_description = "\nUsage: fetch [--from=all/manual/dbworld]\n";
+  boost::program_options::options_description desc(help_description);
+
+  desc.add_options()
+    (HELP, "produce help and usage message")
+    (FROM, boost::program_options::value<std::string>(&fetch_from)->default_value("all"), "choose fetching source")
+    ;
+
+  boost::program_options::variables_map vm;
+  boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
+  boost::program_options::notify(vm);
+
+  if (vm.count(HELP))
+    {
+      std::cerr << desc << std::endl;
+      exit(1);
+    }
+
+
   DBGINIT(std::cerr, Logger::INFO | Logger::ERRO | Logger::VERB | Logger::DEBU);
 
   std::string config_filename = "/Users/minhdt/Documents/Study/findscholarships-website/config.cfg";
@@ -16,10 +40,6 @@ int main()
 
   DBGINFO("Fetching starts...");
 
-  DBGDEBUG("pathTemp     = " << config->pathTemp())
-  DBGDEBUG("inputLinks   = " << config->inputLinks())
-  DBGDEBUG("inputFetched = " << config->inputFetched())
-
   Fetcher fc(config->pathTemp(),
 	     config->pathDatabase(),
 	     config->inputLinks(), 
@@ -27,8 +47,33 @@ int main()
 	     config->markerDbworld(),
 	     config->inputDbworld());
 
-  //fc.fetchMultiple();
-  fc.fetchDbworld();
+  if (fetch_from == "manual")
+    {
+      DBGDEBUG("pathTemp      = " << config->pathTemp())
+      DBGDEBUG("inputLinks    = " << config->inputLinks())
+      DBGDEBUG("inputFetched  = " << config->inputFetched())
+
+      fc.fetchMultiple();
+    }
+  else if (fetch_from == "dbworld")
+    {
+      DBGDEBUG("inputDatabase = " << config->pathDatabase())
+      DBGDEBUG("markerDbworld = " << config->markerDbworld())
+      DBGDEBUG("inputDbworld  = " << config->inputDbworld())
+      fc.fetchDbworld();
+    }
+  else
+    {
+      assert (fetch_from == "all");
+      DBGDEBUG("pathTemp      = " << config->pathTemp())
+      DBGDEBUG("inputDatabase = " << config->pathDatabase())
+      DBGDEBUG("inputLinks    = " << config->inputLinks())
+      DBGDEBUG("inputFetched  = " << config->inputFetched())
+      DBGDEBUG("markerDbworld = " << config->markerDbworld())
+
+      fc.fetchMultiple();
+      fc.fetchDbworld();
+    }
 
   DBGINFO("Fetching finished...");
 }
--- src/findscholarship.cpp
+++ src/findscholarship.cpp
@@ -3,9 +3,12 @@
 #include <sstream>
 #include <string>
 
+#include <boost/program_options.hpp>
+
 #include "utilities/Configuration.hpp"
 #include "utilities/HelperFunctions.hpp"
 #include "utilities/Logger.hpp"
+#include "utilities/ProgramOptions.hpp"
 #include "htmlgen/Database.hpp"
 #include "htmlgen/HtmlGenBase.hpp"
 #include "htmlgen/Manager.hpp"
@@ -15,6 +18,26 @@
 int 
 main (int argc, const char *argv[])
 {
+  std::string from = "all";
+
+  const char *help_description = "\nUsage: fetch [--from=all/manual/dbworld]\n";
+  boost::program_options::options_description desc(help_description);
+
+  desc.add_options()
+    (HELP, "produce help and usage message")
+    (FROM, boost::program_options::value<std::string>(&from)->default_value("all"), "choose fetching source")
+    ;
+
+  boost::program_options::variables_map vm;
+  boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
+  boost::program_options::notify(vm);
+
+  if (vm.count(HELP))
+    {
+      std::cerr << desc << std::endl;
+      exit(1);
+    }
+
   DBGINIT(std::cerr, Logger::INFO | Logger::ERRO | Logger::VERB | Logger::DEBU)
 	
   std::string config_filename = "/Users/minhdt/Documents/Study/findscholarships-website/config.cfg";
@@ -31,8 +54,26 @@ main (int argc, const char *argv[])
   
   DBGINFO("Process batch")
   Manager man;	
-  man.processBatch(config->pathTemp() + config->inputFetched(), db);
-  
+
+  if (from == "manual")
+    {
+      DBGINFO("From manual input...")
+      man.processBatch(config->pathTemp() + config->inputFetched(), db);
+    }
+  else if (from == "dbworld")
+    {
+      DBGINFO("From dbworld...")
+      man.processBatch(config->pathTemp() + config->inputDbworld(), db);
+    }
+  else
+    {
+      assert (from == "all");
+      DBGINFO("From manual input...")
+      man.processBatch(config->pathTemp() + config->inputFetched(), db);
+      DBGINFO("From dbworld...")
+      man.processBatch(config->pathTemp() + config->inputDbworld(), db);
+    }
+
   DBGINFO("Finish!")
     
   return 0;
